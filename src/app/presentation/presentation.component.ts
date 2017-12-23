c9d054e20367347757705375d751b3ed32162a55
Rockman1233 <rocki67@me.com>
2017-12-24 00:48:26 +0300

    forms works

--- src/app/presentation/presentation.component.ts
+++ src/app/presentation/presentation.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 declare var jquery:any;
-declare var $ :any;
+declare var $:any;
 
 
 @Component({
@@ -22,20 +22,22 @@ var left, opacity, scale; //fieldset properties which we will animate
 var animating; //flag to prevent quick multi-click glitches
 
 $(".next").click(function(){
-	if(animating) return false;
-	animating = true;
-	
+    if(animating) return false;
+    animating = true;
+
 	current_fs = $(this).parent();
 	next_fs = $(this).parent().next();
-	
+
 	//activate next step on progressbar using the index of next_fs
 	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-	
+
 	//show the next fieldset
-	next_fs.show(); 
+	next_fs.show();
+    current_fs.hide();
+    animating = false;
 	//hide the current fieldset with style
 	current_fs.animate({opacity: 0}, {
-		step: function(now, mx) {
+		step: function(now) {
 			//as the opacity of current_fs reduces to 0 - stored in "now"
 			//1. scale current_fs down to 80%
 			scale = 1 - (1 - now) * 0.2;
@@ -44,16 +46,16 @@ $(".next").click(function(){
 			//3. increase opacity of next_fs to 1 as it moves in
 			opacity = 1 - now;
 			current_fs.css({
-        'transform': 'scale('+scale+')',
+        'transform': 'scale(' + scale + ')',
         'position': 'absolute'
       });
 			next_fs.css({'left': left, 'opacity': opacity});
-		}, 
-		duration: 800, 
+		},
+		duration: 800,
 		complete: function(){
 			current_fs.hide();
 			animating = false;
-		}, 
+		},
 		//this comes from the custom easing plugin
 		easing: 'easeInOutBack'
 	});
@@ -62,33 +64,35 @@ $(".next").click(function(){
 $(".previous").click(function(){
 	if(animating) return false;
 	animating = true;
-	
+
 	current_fs = $(this).parent();
 	previous_fs = $(this).parent().prev();
-	
+
 	//de-activate current step on progressbar
 	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-	
+
 	//show the previous fieldset
-	previous_fs.show(); 
+	previous_fs.show();
+    current_fs.hide();
+    animating = false;
 	//hide the current fieldset with style
 	current_fs.animate({opacity: 0}, {
-		step: function(now, mx) {
+		step: function(now) {
 			//as the opacity of current_fs reduces to 0 - stored in "now"
 			//1. scale previous_fs from 80% to 100%
 			scale = 0.8 + (1 - now) * 0.2;
 			//2. take current_fs to the right(50%) - from 0%
-			left = ((1-now) * 50)+"%";
+			left = ((1 - now) * 50)+"%";
 			//3. increase opacity of previous_fs to 1 as it moves in
 			opacity = 1 - now;
 			current_fs.css({'left': left});
-			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
-		}, 
-		duration: 800, 
+			previous_fs.css({'transform': 'scale(' + scale + ')', 'opacity': opacity});
+		},
+		duration: 800,
 		complete: function(){
 			current_fs.hide();
 			animating = false;
-		}, 
+		},
 		//this comes from the custom easing plugin
 		easing: 'easeInOutBack'
 	});
@@ -101,7 +105,7 @@ $(".submit").click(function(){
 
   }
 
-  
+
 
 
 
