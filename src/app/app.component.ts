bf6b292e76c9057518ea922e0ab39213ed9a7892
Rockman1233 <rocki67@me.com>
2018-01-03 14:34:43 +0300

    step2 was changed on XS dev.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -53,7 +53,7 @@ export class AppComponent implements OnInit {
     removeFooter() {
         var titlee = this.location.prepareExternalUrl(this.location.path());
         titlee = titlee.slice( 1 );
-        if(titlee === 'signup' || titlee === 'nucleoicons'){
+        if(titlee === 'signup' || titlee === 'nucleoicons' ){
             return false;
         }
         else {
