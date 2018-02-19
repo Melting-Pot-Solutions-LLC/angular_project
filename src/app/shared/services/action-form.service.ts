88f2c5369ba24a279dfd214831dd9de5844b34eb
vtsybulin <vanyatsybulin@gmail.com>
2018-02-19 18:35:59 +0200

    presentation scroll issue fix

--- src/app/shared/services/action-form.service.ts
+++ src/app/shared/services/action-form.service.ts
@@ -29,34 +29,40 @@ export class ActionFormService {
                     console.log('purchase error');
                 }
 
+                if(title_insurance < 228) title_insurance = 228;
+
                 const recording_fee = (actionFormModel.price < 400000) ? actionFormModel.price * 0.011 : actionFormModel.price * 0.014;
                 const document_recording = 180;
 
-                total = title_insurance + recording_fee + document_recording + company_fees;
+                total = title_insurance + recording_fee + document_recording + company_fees + ((actionFormModel.loanAmount > 0)? 150: 0);
 
 
                 console.log("PURCHASE IN DC");
-                console.log("title_insurance - " + title_insurance + " recording_fee - " + recording_fee + " document_recording - " + document_recording + " company_fees - " + company_fees);
+                console.log("title_insurance - " + title_insurance + " recording_fee - " + recording_fee + " document_recording - " + document_recording + " company_fees - " + company_fees + ((actionFormModel.loanAmount > 0)? " +$150 since there is a loan": ""));
 
             } else if (actionFormModel.type === 'Refinance') {
                 let title_insurance = 0;
-                const number_of_thousands = Math.ceil(actionFormModel.price / 1000);
+                const number_of_thousands = Math.ceil(actionFormModel.loanAmount / 1000);
                 if (number_of_thousands < 250) {
                     title_insurance = number_of_thousands * 2.7;
                 } else if (number_of_thousands >= 250 && number_of_thousands < 500) {
-                    title_insurance = 250 * 5.7 + (number_of_thousands - 250) * 2.34;
+                    title_insurance = 250 * 2.7 + (number_of_thousands - 250) * 2.34;
                 } else if (number_of_thousands >= 500 && number_of_thousands < 1000) {
-                    title_insurance = 250 * 5.7 + 250 * 2.34 + (number_of_thousands - 500) * 1.98;
+                    title_insurance = 250 * 2.7 + 250 * 2.34 + (number_of_thousands - 500) * 1.98;
                 } else if (number_of_thousands >= 1000 && number_of_thousands < 5000) {
-                    title_insurance = 250 * 5.7 + 250 * 2.34 + 500 * 1.98 + (number_of_thousands - 1000) * 1.62;
+                    title_insurance = 250 * 2.7 + 250 * 2.34 + 500 * 1.98 + (number_of_thousands - 1000) * 1.62;
                 } else if (number_of_thousands >= 5000 && number_of_thousands < 15000) {
-                    title_insurance = 250 * 5.7 + 250 * 2.34 + 500 * 1.98 + 4000 * 1.62 + (number_of_thousands - 5000) * 0.75;
+                    title_insurance = 250 * 2.7 + 250 * 2.34 + 500 * 1.98 + 4000 * 1.62 + (number_of_thousands - 5000) * 0.75;
                 } else if (number_of_thousands >= 15000) {
-                    title_insurance = 250 * 5.7 + 250 * 2.34 + 500 * 1.98 + 4000 * 1.62 + 10000 * 0.75 +
+                    title_insurance = 250 * 2.7 + 250 * 2.34 + 500 * 1.98 + 4000 * 1.62 + 10000 * 0.75 +
                         (number_of_thousands - 15000) * 0.6;
                 } else {
                     console.log('refinance error 1');
                 }
+
+                if(title_insurance < 101) title_insurance = 101;
+                title_insurance *= 0.7;
+
                 const document_recording = 180;
 
                 console.log("REFINANCE IN DC");
@@ -93,14 +99,14 @@ export class ActionFormService {
 
                 const document_recording = 150;
 
-                const grantor_tax = actionFormModel.price * 0.001;
-                const congestion_tax = actionFormModel.price * 0.0015;
+                // const grantor_tax = actionFormModel.price * 0.001;
+                // const congestion_tax = actionFormModel.price * 0.0015;
                 console.log("PURCHASE IN VA");
-                total = title_insurance + document_recording + company_fees + grantor_tax + congestion_tax +
+                total = title_insurance + document_recording + company_fees +
                     deed_document_recording + loan_document_recording + city_transfer_tax + mortgage_tax +
                     state_transfer_tax + state_mortgage_tax;
 
-                    console.log("title_insurance - " + title_insurance + " document_recording - " + document_recording + " company_fees - " + company_fees);
+                    console.log("title_insurance - " + title_insurance + " document_recording - " + document_recording + " company_fees - " + company_fees + " city_transfer_tax -" + city_transfer_tax + " mortgage_tax - " + mortgage_tax + " state_transfer_tax - "  + state_transfer_tax + " state_mortgage_tax - " + state_mortgage_tax);
 
 
             } else if (actionFormModel.type === 'Refinance') {
@@ -113,7 +119,7 @@ export class ActionFormService {
                 const state_mortgage_tax = actionFormModel.loanAmount * 0.0025;
                 let title_insurance = 0;
 
-                const number_of_thousands = Math.ceil(actionFormModel.price / 1000);
+                const number_of_thousands = Math.ceil(actionFormModel.loanAmount / 1000);
                 if (number_of_thousands < 250) {
                     title_insurance = number_of_thousands * 2.9;
                 } else if (number_of_thousands >= 250 && number_of_thousands < 500) {
@@ -127,6 +133,8 @@ export class ActionFormService {
                 } else {
                     console.log('refinance error VA 1')
                 }
+
+                title_insurance *= 0.7;
 
                 // document_recording = 180
                 total = title_insurance + loan_document_recording + mortgage_tax + state_mortgage_tax + company_fees;
